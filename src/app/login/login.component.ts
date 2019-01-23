b0e67a5a2f498b5db956e5c2b3655993e2bb89cc
Xurmein <wj.pavocrist@gmail.com>
2019-01-23 13:58:28 -0500

    missed this somehow/getting back on my feet

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LoginForm, ActiveUser } from '../models/user.model';
 import { AuthorizationService } from '../services/authorization.service';
 
 @Component({
@@ -7,15 +9,24 @@ import { AuthorizationService } from '../services/authorization.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  
+  private _username : string;
+  private _email : string;
+  private _password : string;
+  
+  public user : ActiveUser; 
+
+  loginForm = new LoginForm(
+    this._username = '',
+    this._email = '',
+    this._password = ''
+  )
+  
   constructor(
     private auth: AuthorizationService,
     private http: HttpClient
   ) { }
-
-  ngOnInit() {
-  }
-
 
 
 
